3f9f412626ac9640f43b32a07897decf37b869ba
Ashka4 <a.kntr93@gmail.com>
2021-12-06 23:38:05 +0300

    Update navbar component

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="navbar">
     <router-link to="/" class="logo"><img src="../assets/images/logo.png"></router-link>
+    <div class="search">
+      <el-autocomplete
+        class="search-input"
+        v-model="state"
+        :fetch-suggestions="querySearchAsync"
+        placeholder="Search for Product"
+        :trigger-on-focus="false"
+        @select="handleSelect"
+        size="medium"
+      />
+      <div class="search-icon">
+        <router-link to="/" class="search-buttton" ><font-awesome-icon :icon="['fa', 'search']" size="lg" /></router-link>
+      </div>
+    </div>
     <div class="account-cart">
       <router-link to="/" class="sign">Sign In/Register</router-link>
       <font-awesome-icon class="account" :icon="['fa', 'user']" size="lg" />
@@ -23,11 +37,32 @@
   .logo {
     margin-left: 40px;
   }
+
+  .search {
+    display: inline-flex;
+    justify-content:flex-end;
+    align-items: center;
+
+    .search-input {
+      width: 500px;
+    }
+    .search-icon {
+      position: absolute;
+      display: flex;
+      color: $primary;
+      padding: 10px;
+
+      & > :hover {
+        color: $secondary;
+      }
+    }
+  }
+
   .account-cart {
     display: flex;
     flex-direction: row;
     gap: 40px;
-    margin-right: 100px;
+    margin-right: 50px;
 
     .sign {
       text-decoration: none;
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,14 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { ElButton, ElRadio, ElInput, ElForm } from 'element-plus'
+import { ElButton, ElRadio, ElInput, ElForm, ElAutocomplete } from 'element-plus'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faShoppingCart, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faUser, faShoppingCart)
+library.add(faUser, faShoppingCart, faSearch)
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
-app.use(ElButton).use(ElRadio).use(ElInput).use(ElForm)
+app.use(ElButton).use(ElRadio).use(ElInput).use(ElForm).use(ElAutocomplete)
 
 app.use(store).use(router).mount('#app')
